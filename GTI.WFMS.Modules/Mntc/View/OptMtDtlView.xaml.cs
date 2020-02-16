5b0c000a11e5b6df78a298f9ee54dad2fad4467e
alsokyun <alsokyun@gmail.com>
2020-02-16 23:11:13 +0900

    시설물가동시간

---
+++ GTI.WFMS.Modules/Mntc/View/OptMtDtlView.xaml.cs
@@ -0,0 +1,163 @@
+﻿using DevExpress.Xpf.Core;
+using GTI.WFMS.Models.Common;
+using GTI.WFMS.Modules.Main;
+using GTI.WFMS.Modules.Mntc.ViewModel;
+using GTIFramework.Common.MessageBox;
+using GTIFramework.Common.Utils.ViewEffect;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace GTI.WFMS.Modules.Mntc.View
+{
+    /// <summary>
+    /// OptMtDtlView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class OptMtDtlView : Window
+    {
+        private string G2_ID;
+        private string FTR_CDE;
+        private string FTR_IDN;
+        private string TAG_ID;
+
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        public OptMtDtlView(string _G2_ID)
+        {
+            InitializeComponent();
+
+            G2_ID = _G2_ID;
+
+            // 테마일괄적용...
+            ThemeApply.Themeapply(this);
+
+            //콤보세팅
+            BizUtil.SetCmbCode(cbCHK_PRD_CDE, "CHK_PRD_CDE", false, "250108");
+
+            // 초기조회
+            InitModel();
+        }
+
+
+
+
+        /// 조회
+        private void InitModel()
+        {
+            //1.시설물정보
+            Hashtable param = new Hashtable();
+            param.Add("sqlId", "SelectOptMtDtlInfo");
+            param.Add("G2_ID", G2_ID);
+
+            DataTable dt = new DataTable();
+            dt = BizUtil.SelectList(param);
+
+            foreach (DataRow row in dt.Rows)
+            {
+                txtFTR_NAM.Text = row["FTR_NAM"].ToString();
+                txtFTR_IDN.Text = row["FTR_IDN"].ToString();
+                txtPRS_NAM.Text = row["PRS_NAM"].ToString();
+                txtATT_NAM.Text = row["ATT_NAM"].ToString();
+                txtCHK_PRD_AMT.Text = row["CHK_PRD_AMT"].ToString();
+                cbCHK_PRD_CDE.EditValue = row["CHK_PRD_CDE"].ToString();
+
+                FTR_CDE = row["FTR_CDE"].ToString();
+                FTR_IDN = row["FTR_IDN"].ToString();
+                try
+                {
+                    TAG_ID = row["TAG_ID"].ToString();
+                }
+                catch (Exception){}
+
+                break;
+            }
+
+            //2.가동시간
+            param = new Hashtable();
+            param.Add("sqlId", "SelectOptMtDtlList");
+            param.Add("TAG_ID", TAG_ID);
+
+            dt = new DataTable();
+            dt = BizUtil.SelectList(param);
+
+            grid.ItemsSource = dt;
+
+
+            //3.점검이력
+            param = new Hashtable();
+            param.Add("sqlId", "SelectOptMtChkHtList");
+            param.Add("FTR_CDE", FTR_CDE);
+            param.Add("FTR_IDN", FTR_IDN);
+
+            dt = new DataTable();
+            dt = BizUtil.SelectList(param);
+
+            grid2.ItemsSource = dt;
+        }
+
+
+
+        //닫기
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            //팝업호출지점으로 리턴
+            DialogResult = true;
+            Close();
+
+        }
+
+       
+        private void OptMtDtlView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.Top = Mouse.GetPosition(this).Y - System.Windows.Forms.Cursor.Position.Y - 6;
+                    this.Left = System.Windows.Forms.Cursor.Position.X - Mouse.GetPosition(this).X + 20;
+
+                    this.WindowState = WindowState.Normal;
+                }
+                this.DragMove();
+            }
+        }
+
+
+        //점검주기저장
+        private void BtnReg_Click(object sender, RoutedEventArgs e)
+        {
+            if (Messages.ShowYesNoMsgBox("저장하시겠습니까?") != MessageBoxResult.Yes) return;
+
+            try
+            {
+                Hashtable param = new Hashtable();
+                param.Add("sqlId", "UpdateOptMt");
+                param.Add("G2_ID", G2_ID);
+                param.Add("CHK_PRD_CDE", cbCHK_PRD_CDE.EditValue);
+                param.Add("CHK_PRD_AMT", txtCHK_PRD_AMT.Text);
+
+                BizUtil.Update(param);
+            }
+            catch (Exception ex)
+            {
+                Messages.ShowErrMsgBox(ex.Message);
+            }
+
+            Messages.ShowOkMsgBox();
+            InitModel();
+        }
+    }
+}
--- GTI.WFMS.Modules/Mntc/View/PdjtStockView.xaml.cs
+++ GTI.WFMS.Modules/Mntc/View/PdjtStockView.xaml.cs
@@ -1,139 +1,63 @@
-﻿using DevExpress.Xpf.Editors;
+﻿using DevExpress.Xpf.Core;
 using GTI.WFMS.Models.Common;
+using GTI.WFMS.Modules.Main;
+using GTI.WFMS.Modules.Mntc.ViewModel;
 using GTIFramework.Common.MessageBox;
 using GTIFramework.Common.Utils.ViewEffect;
 using System;
 using System.Collections;
+using System.ComponentModel;
 using System.Data;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace GTI.WFMS.Modules.Mntc.View
 {
     /// <summary>
-    /// PdjtMngListView.xaml에 대한 상호 작용 논리
+    /// PdjtStockView.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class PdjtMngListView: UserControl
+    public partial class PdjtStockView : Window
     {
+        private string PDH_NUM;
 
-        // 생성자
-        public PdjtMngListView( )
+        /// <summary>
+        /// 생성자
+        /// </summary>
+        public PdjtStockView(string _PDH_NUM)
         {
             InitializeComponent();
+
+            PDH_NUM = _PDH_NUM;
+
+            // 테마일괄적용...
             ThemeApply.Themeapply(this);
 
-            //초기데이터
-            InitDataBinding();
-
-            //초기조회
-            SearchAction();
-
+            // 초기s조회
+            InitModel();
         }
 
 
 
 
-        #region ========== 메소드 ==========
-
-
-        /// <summary>
-        /// 데이터바인딩
-        /// </summary>
-        private void InitDataBinding()
-        {
-            try
-            {
-                // cbPDT_CAT_CDE
-                BizUtil.SetCmbCode(cbPDT_CAT_CDE, "PDT_CAT_CDE", true);
-            }
-            catch (Exception ex)
-            {
-                Messages.ShowErrMsgBoxLog(ex);
-            }
-        }
-
-        /// <summary>
         /// 조회
-        /// </summary>
-        private void SearchAction()
+        private void InitModel()
         {
             DataTable dt = new DataTable();
-
             Hashtable param = new Hashtable();
-            param.Add("sqlId", "SelectPdjtMaMngList");
+         
+            param.Add("sqlId", "SelectPdjtInHtPopList");
+            param.Add("PDH_NUM", PDH_NUM);
 
-            param.Add("PDT_NAM", txtPDT_NAM.Text);
-            param.Add("PDT_MDL_STD", txtPDT_MDL_STD.Text);
-            param.Add("PDT_MNF", txtPDT_MNF.Text);
-            param.Add("PDT_CAT_CDE", cbPDT_CAT_CDE.EditValue.ToString().Trim()); //소모품
-
+            dt = new DataTable();
             dt = BizUtil.SelectList(param);
+
             grid.ItemsSource = dt;
         }
 
 
-        #endregion
 
 
-
-        #region ============ 이벤트핸들러 ============ 
-
-
-        // 검색
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            SearchAction();
-        }
-        //초기화
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            cbPDT_CAT_CDE.SelectedIndex = 0;
-            txtPDT_MDL_STD.Text = "";
-            txtPDT_MNF.Text = "";
-            txtPDT_NAM.Text = "";
-        }
-
-
-        //입고등록
-        private void BtnIn_Click(object sender, RoutedEventArgs e)
-        {
-            string PDH_NUM = "";
-
-            //0.체크박스 체크
-            int cnt = 0;
-            foreach (DataRow dr in ((DataTable)grid.ItemsSource).Rows)
-            {
-                //체크여부
-                if ("Y".Equals(dr["CHK"]))
-                {
-                    cnt++;
-                    try
-                    {
-                        PDH_NUM = dr["PDH_NUM"].ToString();
-                    }
-                    catch (Exception) { }
-                }
-            }
-            if (cnt < 1)
-            {
-                Messages.ShowInfoMsgBox("선택된 항목이 없습니다.");
-                return;
-            }
-            else if (cnt > 1)
-            {
-                Messages.ShowInfoMsgBox("입고대상 항목을 하나만 선택하세요.");
-                return;
-            }
-
-            //1.입고등록 팝업호출
-            PdjtStockView pdjtEnterView = new PdjtStockView(PDH_NUM);
-            if (pdjtEnterView.ShowDialog() is bool)
-            {
-                //팝업종료 후 재조회
-                SearchAction();
-            }
-
-        }
 
 
 
@@ -161,19 +85,6 @@ namespace GTI.WFMS.Modules.Mntc.View
                         break;
                     }
 
-                    //자식데이터여부
-                    int cnt = 0;
-                    try
-                    {
-                        cnt = Convert.ToInt32(dr["CNT"]) ;
-                    }
-                    catch (Exception){}
-                    if (cnt > 0)
-                    {
-                        Messages.ShowInfoMsgBox("선택한 항목중에 입고내역이 있습니다.");
-                        return;
-                    }
-
                 }
                 if (!isChecked)
                 {
@@ -193,8 +104,9 @@ namespace GTI.WFMS.Modules.Mntc.View
                             if ("Y".Equals(((DataTable)grid.ItemsSource).Rows[i]["CHK"]))
                             {
                                 conditions.Clear();
-                                conditions.Add("sqlId", "DeletePdjtMa");
+                                conditions.Add("sqlId", "DeletePdjtInHtPop");
                                 conditions.Add("PDH_NUM", ((DataTable)grid.ItemsSource).Rows[i]["PDH_NUM"].ToString());
+                                conditions.Add("IN_NUM", ((DataTable)grid.ItemsSource).Rows[i]["IN_NUM"].ToString());
 
                                 BizUtil.Update(conditions);
 
@@ -208,7 +120,7 @@ namespace GTI.WFMS.Modules.Mntc.View
                     }
 
                     Messages.ShowOkMsgBox();
-                    SearchAction();
+                    InitModel();
                 }
             }
             catch (Exception ex)
@@ -233,15 +145,13 @@ namespace GTI.WFMS.Modules.Mntc.View
 
                 if (row.RowState == DataRowState.Modified || row.RowState == DataRowState.Added)
                 {
-                    param.Add("sqlId", "SavePdjtMa");
-                    param.Add("PDH_NUM", row["PDH_NUM"]);
+                    param.Add("sqlId", "SavePdjtInHtPop");
+                    param.Add("PDH_NUM", PDH_NUM);
+                    param.Add("IN_NUM", row["IN_NUM"]);
 
-                    param.Add("PDT_CAT_CDE", row["PDT_CAT_CDE"]);
-                    param.Add("PDT_NAM", row["PDT_NAM"]);
-                    param.Add("PDT_MDL_STD", row["PDT_MDL_STD"]);
-                    param.Add("PDT_UNT", row["PDT_UNT"]);
-                    param.Add("PDT_MNF", row["PDT_MNF"]);
-                    param.Add("USE_YN", row["USE_YN"]);
+                    param.Add("IN_AMT", row["IN_AMT"]);
+                    param.Add("IN_YMD", row["IN_YMD"]);
+                    param.Add("IN_ETC", row["IN_ETC"]);
                 }
                 else
                 {
@@ -263,18 +173,8 @@ namespace GTI.WFMS.Modules.Mntc.View
             }
             //저장처리 성공
             Messages.ShowOkMsgBox();
-            SearchAction();
+            InitModel();
         }
-
-
-        //그리드내 콤보구성
-        private void PDT_CAT_CDE_Loaded(object sender, RoutedEventArgs e)
-        {
-            ComboBoxEdit obj = sender as ComboBoxEdit;
-            //구분
-            obj.ItemsSource = BizUtil.GetCmbCode("PDT_CAT_CDE", true);
-        }
-
 
 
         /// <summary>
@@ -306,8 +206,49 @@ namespace GTI.WFMS.Modules.Mntc.View
 
 
 
-        #endregion
 
 
+
+
+
+
+
+
+
+
+
+
+        //닫기
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            //팝업호출지점으로 리턴
+            DialogResult = true;
+            Close();
+
+        }
+ 
+
+        private void PdjtStockView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void Grid_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Mouse.LeftButton == MouseButtonState.Pressed)
+            {
+                if (this.WindowState == WindowState.Maximized)
+                {
+                    this.Top = Mouse.GetPosition(this).Y - System.Windows.Forms.Cursor.Position.Y - 6;
+                    this.Left = System.Windows.Forms.Cursor.Position.X - Mouse.GetPosition(this).X + 20;
+
+                    this.WindowState = WindowState.Normal;
+                }
+                this.DragMove();
+            }
+        }
     }
 }
